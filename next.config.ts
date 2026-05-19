e6a802c76ef3794f207b8dc79a8862d67efe56f4
Gregorick <gregorick.liriano@gmail.com>
2026-05-19 16:15:04 -0400

    next link

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 👇 Esto le dice a Next.js que vivirá en esta subcarpeta en cPanel
+  basePath: '/fieldstone-embroider', 
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 's7d9.scene7.com' }, 
       { protocol: 'https', hostname: 'view.sanmar.com' }, 
       { protocol: 'https', hostname: 'www.sanmar.com' },  
       { protocol: 'https', hostname: 'cdn.sanmar.com' },  
-      { protocol: 'https', hostname: 'cdnm.sanmar.com' }, // <-- ¡AÑADE ESTA LÍNEA!
+      { protocol: 'https', hostname: 'cdnm.sanmar.com' }, 
       { protocol: 'https', hostname: 'placehold.co' },
     ],
   }
